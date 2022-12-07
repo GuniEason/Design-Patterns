b9ddca6f866c7223deb9bd16fe4785c817174428
GuniEason <gunieason.163.com>
2022-12-07 17:59:11 +0800

    memento pattern practice

---
+++ src/main/java/com/p6/pattern/memento/ArticleMemento.java
@@ -0,0 +1,43 @@
+package com.p6.pattern.memento;
+
+/**
+ * @project Design-Patterns
+ * @package com.p6.pattern.editor
+ * @className ArticleMemento
+ * @author: zhuyf
+ * @Date:2022/12/07 17:54
+ * @Description: //
+ */
+public class ArticleMemento {
+
+    private String title;
+    private String content;
+    private String imgs;
+
+    public ArticleMemento(String title, String content, String imgs) {
+        this.title = title;
+        this.content = content;
+        this.imgs = imgs;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getImgs() {
+        return imgs;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleMemento{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", imgs='" + imgs + '\'' +
+                '}';
+    }
+}
